31c35d960028a4439ff674551ca8be5820bca95e
sunbeam-pdac <piyushshinde1902@gmail.com>
2024-06-26 18:22:18 +0530

    BACKEND API: Get All Properties of owner

--- codebase/backend/tourify/src/main/java/com/project/tourify/services/IPropertyService.java
+++ codebase/backend/tourify/src/main/java/com/project/tourify/services/IPropertyService.java
@@ -1,8 +1,12 @@
 package com.project.tourify.services;
 
+import java.util.List;
+
 import com.project.tourify.dtos.PropertyDto;
 
 public interface IPropertyService{
 
 	PropertyDto addProperty(PropertyDto propertyDto);
+	
+	List<PropertyDto> getAllProperties();
 }
