bdfaf2d3a2293d380464e3387d03d8d14ae7c0d9
sunbeam-pdac <piyushshinde1902@gmail.com>
2024-06-27 15:36:03 +0530

    BACKEND API: Get All Places

---
+++ codebase/backend/tourify/src/main/java/com/project/tourify/dtos/PlaceDto.java
@@ -0,0 +1,21 @@
+package com.project.tourify.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceDto {
+
+	private Long id;
+	
+	private String place;
+}
